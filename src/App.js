1834d3d93b07a7e022348535dbf6ed46b8a2ab0c
Nick Paz <31939425+nickpazfernande@users.noreply.github.com>
2023-02-02 18:57:24 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Acá va a tar SpotifyClone 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://developer.spotify.com/documentation/web-api/reference/#/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Documentación API
         </a>
       </header>
     </div>
