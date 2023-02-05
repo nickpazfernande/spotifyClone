7c601f6a354fa487084da1a911e7bd7ed255551a
Nick Paz <31939425+nickpazfernande@users.noreply.github.com>
2023-02-04 21:54:02 -0300

    save token and search tracks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useSpotifyApi from "./hooks/useSpotifyApi";
 import "./App.css";
 import cors from "cors";
 
 function App() {
+  const [data, setData] = useState({});
   const token = "64371d1b7bdc43b99bd945817c7d0429";
   const url = "https://api.spotify.com/v1/me/tracks";
-  const { getToken } = useSpotifyApi(url, token);
+  const { getToken, searchTrack } = useSpotifyApi(url, token);
 
   const changeUser = () => {
     // refreshData();
@@ -27,11 +28,10 @@ function App() {
         >
           Documentación API
         </a>
-        {/* <div>
-          {loading && <p>Loading...</p>}
-          {!loading && <p>Nombre: {data.results[0].name.first}</p>}
-        </div> */}
-        <button onClick={() => changeUser()}>Change user</button>
+        <input type="text" onChange={(e) => setData(e.target.value)} />
+        <button onClick={() => searchTrack(data)}>Buscar</button>
+        {/* <button onClick={() => getToken()}>Get token</button>
+        <button onClick={() => changeUser()}>Change user</button> */}
       </header>
     </div>
   );
--- src/hooks/useSpotifyApi.jsx
+++ src/hooks/useSpotifyApi.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 
-const client_secret =  process.env.REACT_APP_CLIENT_SECRET;
+const client_secret = process.env.REACT_APP_CLIENT_SECRET;
 const client_id = process.env.REACT_APP_CLIENT_ID;
 const urlSpotify = process.env.REACT_APP_URLSPOTIFY;
 
+const urlTrack = "https://api.spotify.com/v1/tracks/";
+
 const useSpotifyApi = (url, token) => {
   const [data, setData] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -25,13 +28,34 @@ const useSpotifyApi = (url, token) => {
         "&client_secret=" +
         client_secret,
     };
-    fetch(urlSpotify, authParameters)
+    fetch(urlSpotify, authParameters) //next save the token
       .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        setAccessToken(data.access_token);
+        console.log(accessToken);
+      })
       .catch((error) => console.log(error));
   };
 
-  return { getToken };
+  const searchTrack = (search) => {
+    url =
+      "https://api.spotify.com/v1/search?q="+ search + "*&type=album,artist,playlist,track&limit=50&offset=0&market=UY";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+        setLoading(false);
+        setError(null);
+      });
+  };
+
+  return { getToken, searchTrack };
 };
 
 export default useSpotifyApi;
